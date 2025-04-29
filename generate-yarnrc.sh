9aba4968b2f61b1898626da5d669e540a7ee1e1b
vpvnguyen <vpvnguyen@gmail.com>
2025-04-28 23:38:19 -0700

    chore: bump version

---
+++ generate-yarnrc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Define the file path
+YARN_RC="./.yarnrc.yml"
+NPM_RC="./.npmrc"
+TOKEN_VALUE="${{ secrets.GITHUB_TOKEN }}"
+
+# Check if the file exists, if so, delete it
+if [ -f "$NPM_RC" ]; then
+  echo "File $NPM_RC exists. Deleting it..."
+  rm "$NPM_RC"
+fi
+
+# Check if the file exists, if so, delete it
+if [ -f "$YARN_RC" ]; then
+  echo "File $YARN_RC exists. Deleting it..."
+  rm "$YARN_RC"
+fi
+
+# Check if the file exists, if not, create it
+if [ ! -f "$YARN_RC" ]; then
+  echo "File $YARN_RC does not exist. Creating it..."
+  touch "$YARN_RC"
+  echo "nodeLinker: node-modules" >> "$YARN_RC"
+  echo "npmScopes:" >> "$YARN_RC"
+  echo "  vpvnguyen:" >> "$YARN_RC"
+  echo "    npmRegistryServer: \"https://npm.pkg.github.com\"" >> "$YARN_RC"
+  echo "    npmAlwaysAuth: true" >> "$YARN_RC"
+fi
